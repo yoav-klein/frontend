b81d44a66490100a4c1b769f3552ad78e816ad6c
Yoav <yoavklein25@gmail.com>
2026-06-11 13:12:28 +0300

    Toast

---
+++ toast/README.md
@@ -0,0 +1,6 @@
+# Toast Notifications
+---
+
+Based on:
+* https://www.youtube.com/watch?v=lsCnPWUgNQM
+* https://www.youtube.com/watch?v=sTL7KCOf3kg
---
+++ toast/main.js
@@ -0,0 +1,5 @@
+
+const toast = document.querySelector('.toast');
+document.querySelector('#show-toast').addEventListener('click', () => {
+    toast.classList.add('show');
+});
